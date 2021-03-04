950761856ab96accbaca91f040a4321ef99ffcd5
Luzciel <luzcielmontesinos@gmail.com>
2021-03-04 11:58:10 -0300

    funciones que imprimen las opciones file/validate/stats/validateStats

--- index.js
+++ index.js
@@ -3,75 +3,39 @@ const path = require('path'); //El path módulo proporciona utilidades para trab
 const http = require('http');
 const clc = require('cli-color');
 const markdownIt = require('markdown-it')();
-const  jsdom  =  require ( "jsdom" ) ; 
-const  {  JSDOM  }  =  jsdom ;
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 const fetchUrl = require("fetch").fetchUrl;
 const resolve = require('path').resolve;
 const { rejects } = require('assert');
+const columns = require('cli-color/columns');
 // resolve('../../bb/tmp.txt')
+
 let allLinks = []
-let onlyLink = []
 let detailsLinks = {};
-let brokenLiks = [];
 
 const redColor = clc.red.bold;
 const greenColor = clc.green.italic;
+const greenColorBold = clc.green.bold;
 const blueColor = clc.blue;
 const magentaColor = clc.magenta;
-const cyanColor = clc.cyan;
+const cyanColor = clc.cyan.bold;
 
 const allowedExtensions = /(.md|.markdown|.mdown|.mkdn|.mkd|.mdwn|.mdtxt|.text|.Rmd )$/i;  // Extenciones permitidas  Porque sucede esto??
 
-// const filePath = (fileRoute) =>{
-//   // return new Promise((resolve, reject) => {
-//   fs.readFile(fileRoute, { encoding: 'utf8' }, (err, data) => { // fs.readFile()método se utiliza para leer archivos en su computadora. Este devuleve un objeto Buffer (secuencia de bytes de longitud fija)
-//   // Si no hay errores, 'data' será un objeto Buffer de Node.js. Al igual que pasa con readFileSync(), puedes pasar 'utf8' como segundo parámetro y
-//   // luego el callback como tercero de modo de que data sea un String y no un Buffer.
-//     if (data) {
-//       const str = data.toString();
-//       getLink(str, fileRoute)
-//       if(allLinks.length > 1) {
-//         allLinks.forEach((link) => {
-//         // console.log(cyanColor(link.file), greenColor(link.href), blueColor(link.text));
-//         linkStatus(link.href)
-//         .then((res) => {
-//           console.log(res, greenColor(link.href), blueColor(link.text))
-//         })
-//         .catch(err =>{
-//           console.log(err);
-//         })
-//         })
-//       }
-//       else{
-//         console.log(redColor('No se encontaron documentos con links'));
-//       }
-//     } else {
-//       console.log(redColor('Error: Ruta Invalida'));
-//       console.log(err.message);
-//     }
-//   });
-// // }) 
-// }
-const filePath = (fileRoute) =>{
+
+const filePath = (fileRoute) => {
   return new Promise((resolve, reject) => {
-  fs.readFile(fileRoute, { encoding: 'utf8' }, (err, data) => { // fs.readFile()método se utiliza para leer archivos en su computadora. Este devuleve un objeto Buffer (secuencia de bytes de longitud fija)
-  // Si no hay errores, 'data' será un objeto Buffer de Node.js. Al igual que pasa con readFileSync(), puedes pasar 'utf8' como segundo parámetro y
-  // luego el callback como tercero de modo de que data sea un String y no un Buffer.
-    if (data) {
-      const str = data.toString();
-      // getLink(str, fileRoute)
-      resolve(getLink(str, fileRoute ))
-    } else {
-      reject(console.log(redColor('Error: Ruta Invalida')))
-    }
-  });
-})
+    fs.readFile(fileRoute, { encoding: 'utf8' }, (err, data) => { // fs.readFile()método se utiliza para leer archivos en su computadora. Este devuleve un objeto Buffer (secuencia de bytes de longitud fija)
+      if (data) {
+        const str = data.toString();
+        resolve(getLink(str, fileRoute))
+      } else {
+        reject(console.log(redColor('Error: Ruta Invalida')))
+      }
+    });
+  })
 }
-
-
-
-
-
 
 // Funcion que obtiene el link, el texto y el archivo 
 const getLink = (fileMd, pathParameter) => {
@@ -79,114 +43,165 @@ const getLink = (fileMd, pathParameter) => {
   const domContent = new JSDOM(tokens); // 
   const links = Array.from(domContent.window.document.querySelectorAll('a'));
   allLinks = [];
-  links.forEach((link) => { 
-    if(link.href.includes("http")){
+  links.forEach((link) => {
+    if (link.href.includes("http")) {
       detailsLinks = {
         href: link.href,
         text: link.text,
         file: pathParameter,
       }
-      // onlyLink.push(link.href)
       allLinks.push(detailsLinks)
-      
     }
   })
   return allLinks
 }
 
 
+const opcionFile = (folder) => {
+  filePath(folder)
+    .then(res => {
+      res.map(element => {
+        console.log(cyanColor(element.file), greenColor(element.href), blueColor(element.text));
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
 
+
+const opcionValidate = (folder) => {
+  filePath(folder)
+    .then(res => {
+      res.forEach((link) => {
+        linkStatus(link.href)
+          .then(response => {
+            if (response >= 400) {
+              console.log(cyanColor(link.file), greenColor(link.href), redColor(response), link.text, redColor('Fail'))
+            } else {
+              console.log(cyanColor(link.file), greenColor(link.href), blueColor(response), link.text, blueColor('OK'))
+            }
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+
+const opcionStats = (folder) => {
+  filePath(folder)
+    .then(res => {
+      console.log(cyanColor('TOTAL:', res.length))
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+
+const opcionStatsValidate = (folder) => {
+  filePath(folder)
+    .then(res => {
+      const broken = [];
+      res.forEach((link) => {
+        broken.push(linkStatus(link.href));
+      })
+      Promise.all(broken)
+        .then(response => {
+          const result = response.filter(status => {
+            if (status >= 400) {
+              return status;
+            }
+          })
+          console.log(cyanColor('Total:', res.length))
+          console.log(redColor('Broken:', result.length))
+        })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+
+
+// Funcion que obtiene el estatus del Link
 const linkStatus = (link) => {
   return new Promise((resolve, rejects) => {
-     fetchUrl(link, (error, meta) => {
-       if(meta){
-        // brokenLiks.push(element.href)
+    fetchUrl(link, (error, meta) => {
+      if (meta) {
         resolve(meta.status);
-       } else {
+      } else {
         rejects(error)
-       }
+      }
     })
+  })
+}
 
-  // })
- })
+
+
+const unico = (folder) => {
+  const unique = [];
+  filePath(folder)
+    .then(res => {
+      res.map((link) => {
+        unique.push(link.href)
+      })
+      const mySet = new Set(unique);
+      console.log(greenColorBold("Unique:", mySet.size))
+    })
+    .catch(err => {
+      console.log(err)
+    })
 }
 
 
 
 
-  // linkStatus(allLinks)
-  //   .then(res =>{
-  //     console.log(res)
-  //   })
-  //   .catch(err =>{
-  //     console.log(err);
-  //   })
-
-
-
-// const linkStatus = (link) => {
-//  link.forEach(element =>{
-//   fetchUrl(element.href, (error, meta, body) => {
-//     if(meta.status >= 400) {
-//       // detailsLinks = {
-//       //   href: link.href,
-//       //   text: link.text,
-//       //   file: pathParameter,
-//       // }
-//       brokenLiks.push(element.href)
-//       // console.log(55, element.href)
-//       // console.log('______________________________________')
-//       // console.log(element.href, element.file, meta.status, element.text, redColor('Fail'))
-//     } else {
-//       console.log('______________________________________')
-//       console.log(element.href, element.file, meta.status, element.text, 'Ok')
-//     }
-//   })
-//   console.log(333, brokenLiks)
-//  })
-// }
-
-
 const directoryPath = (pachParameter) => {
   return new Promise((resolve, reject) => {
-  fs.readdir(pachParameter, (err, data) => {
-    if(err){
-      reject(console.log(clc.red('Directorio no encontrado')));
-      console.log(err.me)
-    } else {
-      filter =[]
-      data.forEach(file => {
-        if(allowedExtensions.exec(path.extname(file))){ //El método exec() ejecuta una busqueda sobre las coincidencias de una expresión regular en una cadena especifica. Devuelve el resultado como array, o null.
-          //path.extname () devuelve la extensión del path, desde la última aparición del carácter .(punto)  (ejemplo .js , .txt)
-          filter.push(file)
-        }
-      });
-      if(filter.length === 0 ) { // Object.entries devuelve una lista con las claves y valores del objeto. En caso el objeto sea vacío va a devolver un array vacío.
-        console.log(redColor('No se econtraron archivos con extension Markdown'));
+    fs.readdir(pachParameter, (err, data) => {
+      if (err) {
+        reject(console.log(clc.red('Directorio no encontrado')));
+        console.log(err.me)
       } else {
-        filter.forEach(fileMarkdown=> {
-          const absolutePath = `${pachParameter}\\${fileMarkdown}`;
-          resolve(absolutePath)
-          // resolve(filePath(absolutePath)
-          // .then(res => {
-          //   console.log(res);
-          // })
-          // .catch(err =>{
-          //   console.log(err)
-          // }))
+        filter = []
+        data.forEach(file => {
+          if (allowedExtensions.exec(path.extname(file))) { //El método exec() ejecuta una busqueda sobre las coincidencias de una expresión regular en una cadena especifica. Devuelve el resultado como array, o null.
+            //path.extname () devuelve la extensión del path, desde la última aparición del carácter .(punto)  (ejemplo .js , .txt)
+            filter.push(file)
+          }
+        });
+        if (filter.length === 0) { // Object.entries devuelve una lista con las claves y valores del objeto. En caso el objeto sea vacío va a devolver un array vacío.
+          console.log(redColor('No se econtraron archivos con extension Markdown'));
+        } else {
+          filter.forEach(fileMarkdown => {
+            const absolutePath = `${pachParameter}\\${fileMarkdown}`;
+            resolve(filePath(absolutePath)
+              .then(res => {
+                console.log(333, res);
+              })
+              .catch(err => {
+                console.log(err)
+              }))
           })
+        }
       }
-    }
-  });
-})
-} 
-
-
-
-
+    });
+  })
+}
 
 
 module.exports = {
   directoryPath,
-  filePath
+  filePath,
+  opcionFile,
+  opcionValidate,
+  opcionStats,
+  opcionStatsValidate,
+  unico
 }
--- mdLinks.js
+++ mdLinks.js
@@ -1,32 +1,48 @@
-// const welcome = 'HELLO WORLD'
-// console.log(welcome)
 const fs = require('fs'); // fs es el módulo del sistema de archivos Node.js le permite trabajar con el sistema de archivos en su computadora.
 const clc = require('cli-color');
 // const directoryPath = require('./index')
-const {filePath, directoryPath} = require('./index')
+// const { fetchUrl } = require('fetch');
+const {filePath, directoryPath, opcionFile, opcionValidate, opcionStats, opcionStatsValidate, unico} = require('./index');
 
 const folder = process.argv[2];  //comienza con 2 porque los dos primeros elementos en la matriz process.argv siempre es['path/to/node.exe', 'path/to/js/file', ...]
-const redColor = clc.red.bold;
-const greenColor = clc.green.italic;
 const optionsArgv = process.argv[3];
 const twoOptionsArgv = process.argv[4];
+const redColor = clc.red.bold;
+const greenColor = clc.green.italic;
+
+
+
+
+const options = (folderParameter, optionsParameter, twoOptionsParameter) => {
+  if (folderParameter && !optionsParameter ) {
+    opcionFile(folderParameter)
+  } else if (optionsParameter === '--validate' && !twoOptionsParameter) {
+      opcionValidate(folderParameter)
+
+  } else if (optionsParameter == '--stats' && !twoOptionsParameter) {
+    opcionStats(folderParameter)
+    unico(folderParameter)
+
+  } else if (optionsParameter == '--stats' && twoOptionsParameter === '--validate' || optionsParameter === '--validate' &&  twoOptionsParameter === '--stats' ) {
+    opcionStatsValidate(folderParameter)
+    unico(folderParameter)
+
+  } else {
+    console.log("Error instrucciones");
+  }
+}
+
+
 
 // const commandLineInterface = (folderParameter) => {
   if (fs.lstatSync(folder).isFile()) {
       console.log(greenColor("Es un archivo"))
-      filePath(folder)
-      .then(res => {
-        // console.log(res)
-        options(res, optionsArgv, folder, twoOptionsArgv );
-      })
-      .catch(err =>{
-        console.log(err)
-      })
+        options(folder, optionsArgv, twoOptionsArgv );
     } else {
       console.log(greenColor("Es una carpeta"))
       directoryPath(folder)
       .then(rest => {
-        console.log(rest)
+        // console.log(666666, rest)
         filePath(rest)
         .then(res => {
           // console.log(res)
@@ -44,22 +60,3 @@ const twoOptionsArgv = process.argv[4];
 // }
 // }
 
-const options = (res, optionsParameter, folderParameter, twoOptionsParameter) => {
-  if (folderParameter && !optionsParameter ) {
-    console.log(111, res)
-    // commandLineInterface(folderParameter);
-  } else if (optionsParameter === '--validate' && !twoOptionsParameter) {
-      console.log("entro al --validate");
-
-  } else if (optionsParameter == '--stats' && !twoOptionsParameter) {
-    console.log("entro al -stats");
-
-  } else if (optionsParameter == '--stats' && twoOptionsParameter === '--validate' || optionsParameter === '--validate' &&  twoOptionsParameter === '--stats' ) {
-    console.log("entro al --stats --validate");
-
-  } else {
-    console.log("Error instrucciones");
-  }
-  }
-
-// options(optionsArgv, folder, twoOptionsArgv );
